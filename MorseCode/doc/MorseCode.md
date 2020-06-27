68ae8053d534bccd5cf6e07627e622e993b1c59b
david2du <david2du@163.com>
2020-06-27 08:45:06 +0800

    Version 1 --output 0

---
+++ MorseCode/doc/MorseCode.md
@@ -0,0 +1,13 @@
+# MRRoutine
+
+## Descriptions
+
+## Function 
+
+## Interfaces
+
+### Intelligental Schedule Choosing Project (ISCP)
+
+1. The smaller the grade (PIR * t), try to do it first 
+2. Try not to do more than three FQs in a row 
+3. The vector<> schedule  :You can refer to it and only change status of type int
---
+++ MorseCode/src/MorseCode.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : MorseCode.cpp
+// Author      : David2du
+// Version     : Version 1
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << 0 << endl;
+
+	return 0;
+}
