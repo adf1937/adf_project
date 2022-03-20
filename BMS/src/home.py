255463e88fca9991596eb9637ee519ede2bde0a2
david2du <david2du@163.com>
2022-03-20 23:22:31 +0800

    init

--- BMS/src/home.py
+++ BMS/src/home.py
@@ -30,7 +30,7 @@ class home():
         monty.grid(column=0, row=0, padx=8, pady=4)
 
         # Changing our Label
-        ttk.Label(monty, text="输入文字:").grid(column=0, row=0, sticky='W')
+        ttk.Label(monty, text="书名").grid(column=0, row=0, sticky='W')
 
         # Adding a Textbox Entry widget
         name = tk.StringVar()
@@ -38,7 +38,7 @@ class home():
         nameEntered.grid(column=0, row=1, sticky='W')
 
         # Adding a Button
-        action = ttk.Button(monty, text="点击之后\n按钮失效",
+        action = ttk.Button(monty, text="借阅",
                             width=10, command=self.clickMe)
         action.grid(column=2, row=1, rowspan=2, ipady=7)
 
@@ -68,25 +68,33 @@ class home():
         monty.grid(column=0, row=0, padx=8, pady=4)
 
         # Changing our Label
-        ttk.Label(monty, text="输入文字:").grid(column=0, row=0, sticky='W')
+        ttk.Label(monty, text="书名").grid(column=0, row=0, sticky='W')
 
         # Adding a Textbox Entry widget
-        name = tk.StringVar()
-        nameEntered = ttk.Entry(monty, width=12, textvariable=name)
-        nameEntered.grid(column=0, row=1, sticky='W')
+        bookname = tk.StringVar()
+        booknameEntered = ttk.Entry(monty, width=12, textvariable=bookname)
+        booknameEntered.grid(column=0, row=1, sticky='W')
+
+        # Changing our Label
+        ttk.Label(monty, text="借阅者").grid(column=1, row=0, sticky='W')
+
+        # Adding a Textbox Entry widget
+        username = tk.StringVar()
+        usernameEntered = ttk.Entry(monty, width=12, textvariable=username)
+        usernameEntered.grid(column=1, row=1, sticky='W')
 
         # Adding a Button
-        action = ttk.Button(monty, text="点击之后\n按钮失效",
-                            width=10, command=self.clickMe)
+        action = ttk.Button(monty, text="查找",
+                            width=10, command=self.search_m)
         action.grid(column=2, row=1, rowspan=2, ipady=7)
 
-        ttk.Label(monty, text="请选择一本书:").grid(column=1, row=0, sticky='W')
+        ttk.Label(monty, text="请选择一本书:").grid(column=0, row=2, sticky='W')
 
         # Adding a Combobox
         book = tk.StringVar()
         bookChosen = ttk.Combobox(monty, width=12, textvariable=book)
         bookChosen['values'] = ('平凡的世界', '亲爱的安德烈', '看见', '白夜行')
-        bookChosen.grid(column=1, row=1)
+        bookChosen.grid(column=0, row=3)
         bookChosen.current(0)  # 设置初始显示值，值为元组['values']的下标
         bookChosen.config(state='readonly')  # 设为只读模式
 
@@ -109,27 +117,25 @@ class home():
         # Modified Button Click Function
 
         # Changing our Label
-        ttk.Label(monty, text="输入文字:").grid(column=0, row=0, sticky='W')
+        ttk.Label(monty, text="用户名").grid(column=0, row=0, sticky='W')
 
         # Adding a Textbox Entry widget
         name = tk.StringVar()
         nameEntered = ttk.Entry(monty, width=12, textvariable=name)
-        nameEntered.grid(column=0, row=1, sticky='W')
+        nameEntered.grid(column=1, row=0, sticky='W')
+
+        # Changing our Label
+        ttk.Label(monty, text="密码").grid(column=0, row=1, sticky='W')
+
+        # Adding a Textbox Entry widget
+        name = tk.StringVar()
+        nameEntered = ttk.Entry(monty, width=12, textvariable=name)
+        nameEntered.grid(column=1, row=1, sticky='W')
 
         # Adding a Button
-        action = ttk.Button(monty, text="点击之后\n按钮失效",
+        action = ttk.Button(monty, text="添加",
                             width=10, command=self.clickMe)
-        action.grid(column=2, row=1, rowspan=2, ipady=7)
-
-        ttk.Label(monty, text="请选择一本书:").grid(column=1, row=0, sticky='W')
-
-        # Adding a Combobox
-        book = tk.StringVar()
-        bookChosen = ttk.Combobox(monty, width=12, textvariable=book)
-        bookChosen['values'] = ('平凡的世界', '亲爱的安德烈', '看见', '白夜行')
-        bookChosen.grid(column=1, row=1)
-        bookChosen.current(0)  # 设置初始显示值，值为元组['values']的下标
-        bookChosen.config(state='readonly')  # 设为只读模式
+        action.grid(column=2, row=2, rowspan=2, ipady=7, sticky='N')
 
         # 一次性控制各控件之间的距离
         for child in monty.winfo_children():
@@ -138,6 +144,9 @@ class home():
         action.grid(column=2, row=1, rowspan=2, padx=6)
 
     def clickMe(self):
+        pass
+
+    def search_m(self):
         pass
 
 
