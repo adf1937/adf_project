3d1482350da1968c8d787a437c40356e734809c4
andydu <13816390866@163.com>
2022-03-20 17:34:05 +0800

    rename src

--- BMS/src/home.py
+++ BMS/src/home.py
@@ -1,100 +1,47 @@
-from tkinter import *
-from job import ren, kao, gong, about
+# -*- coding: utf-8 -*-
+import tkinter as tk  # 装载tkinter模块,用于Python3
+from tkinter import ttk  # 装载tkinter.ttk模块,用于Python3
+from bmservice import *
+from bbservice import *
+from busevice import *
 
 
 class home():
     def __init__(self, master=None):
         self.root = master
-        self.root.geometry('400x250+888+444')
-        self.jobtxt = ''  # 用这个变量记下窗体的标题
-        self.createPage()
+        w, h = self.root.maxsize()
+        self.root.geometry("{}x{}+0+0".format(w, h))
+        self.root.resizable(0, 0)
+        # self.root.geometry('400x250+888+444')
+        self.createNotebook()
 
-    def createPage(self):
+    def createNotebook(self):
+        self.tabControl = ttk.Notebook(self.root)  # 创建Notebook
 
-        menubar = Menu(self.root)
-        A = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label='图书借还管理', menu=A)
-        A.add_command(label='借书', command=self.book_borrow_m)
-        A.add_command(label='还书', command=self.book_return_m)
-        A.add_command(label='延期', command=self.book_extend_m)
-        A.add_command(label='关于', command=about)
-        A.add_command(label='退出', command=self.root.destroy)
+        self.createBBPage()
+        self.createBMPage()
+        self.createBUPage()
 
-        A = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label='图书维护管理', menu=A)
-        A.add_command(label='图书查询', command=self.book_search_m)
-        A.add_command(label='图书增删', command=self.book_addremove_m)
-        A.add_command(label='图书修改', command=self.book_modify_m)
-        A.add_command(label='关于', command=about)
-        A.add_command(label='退出', command=self.root.destroy)
+        self.tabControl.pack(expand=1, fill="both")
+        self.tabControl.select(0)  # 选择tab1
 
-        A = Menu(menubar, tearoff=0)
-        menubar.add_cascade(label='用户管理', menu=A)
-        A.add_command(label='用户维护', command=self.user_maintainence_m)
-        A.add_command(label='关于', command=about)
-        A.add_command(label='退出', command=self.root.destroy)
+    def createBBPage(self):
+        # tab = tk.Frame(self.tabControl, bg='blue')  # 增加新选项卡
+        tab = bbservice(self.tabControl)
+        self.tabControl.add(tab, text='借阅管理')  # 把新选项卡增加到Notebook
 
-        self.root.config(menu=menubar)
+    def createBMPage(self):
+        #tab = tk.Frame(self.tabControl, bg='yellow')
+        tab = bmservice(self.tabControl)
+        self.tabControl.add(tab, text='图书管理')
 
-        self.welcome()
-
-    def welcome(self):
-        pass
-
-    def book_borrow_m(self):  # 执行人员管理菜单
-        if self.jobtxt != '图书借阅管理':  # 根据窗体标题来决定否则执行这个菜单功能
-            if self.jobtxt == '图书维护管理':  # 如果要切换，就先根据窗体标签把现在的框架卸载掉
-                self.kaopage.destroy()
-            if self.jobtxt == '用户管理':  # 如果要切换，就先根据窗体标签把现在的框架卸载掉
-                self.gongpage.destroy()
-
-            self.renpage = ren(self.root)  # 调用job.py的ren类，显示图书借阅管理界面
-            self.renpage.pack()
-            self.root.title('图书借阅管理')
-            self.jobtxt = '图书借阅管理'  # 记下窗体标题
-
-    def book_return_m(self):  # 执行图书维护管理菜单
-        if self.jobtxt != '图书维护管理':
-            if self.jobtxt == '图书借阅管理':
-                self.renpage.destroy()
-            if self.jobtxt == '用户管理':
-                self.gongpage.destroy()
-
-            self.kaopage = kao(self.root)
-            self.kaopage.pack()
-            self.root.title('图书维护管理')
-            self.jobtxt = '图书维护管理'
-
-    def book_extend_m(self):  # 执行图书维护管理菜单
-        if self.jobtxt != '图书维护管理':
-            if self.jobtxt == '图书借阅管理':
-                self.renpage.destroy()
-            if self.jobtxt == '用户管理':
-                self.gongpage.destroy()
-
-            self.kaopage = kao(self.root)
-            self.kaopage.pack()
-            self.root.title('图书维护管理')
-            self.jobtxt = '图书维护管理'
-
-    def about(self):  # 调用job.py里的about函数，弹出窗体
-        about()
-
-    def book_search_m(self):
-        pass
-
-    def book_addremove_m(self):
-        pass
-
-    def book_modify_m(self):
-        pass
-
-    def user_maintainence_m(self):
-        pass
+    def createBUPage(self):
+        #tab = tk.Frame(self.tabControl, bg='green')
+        tab = buservice(self.tabControl)
+        self.tabControl.add(tab, text='用户管理')
 
 
 if __name__ == "__main__":
-    root = Tk()
+    root = tk.Tk()
     home(root)
     root.mainloop()
-    pass
