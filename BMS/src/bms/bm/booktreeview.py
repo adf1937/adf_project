9075e38ad50fe44e1d77e7fd5f27e01e6eb189e0
andydu <13816390866@163.com>
2022-04-09 17:01:00 +0800

    name chang for modification

--- BMS/src/bms/bm/booktreeview.py
+++ BMS/src/bms/bm/booktreeview.py
@@ -65,48 +65,51 @@ class booktreeview ():
         # Set up the window's other attributes and geometry
         ####
 
-        col1Lbl = Label(win, text=self.table.heading(0)["text"])
-        col1Ent = Entry(win)
-        col1Ent.insert(0, values[0])  # Default is column 1's current value
-        col1Lbl.grid(row=0, column=0)
-        col1Ent.grid(row=1, column=0)
+        lblBook = Label(win, text=self.table.heading(0)["text"])
+        entBook = Entry(win)
+        entBook.insert(0, values[0])  # Default is column 1's current value
+        lblBook.grid(row=0, column=0)
+        entBook.grid(row=1, column=0)
 
-        col2Lbl = Label(win, text=self.table.heading(1)["text"])
-        col2Ent = Entry(win)
-        col2Ent.insert(0, values[1])  # Default is column 2's current value
-        col2Lbl.grid(row=0, column=1)
-        col2Ent.grid(row=1, column=1)
+        lblBookSN = Label(win, text=self.table.heading(1)["text"])
+        entBookSN = Entry(win)
+        entBookSN.insert(0, values[1])  # Default is column 2's current value
+        lblBookSN.grid(row=0, column=1)
+        entBookSN.grid(row=1, column=1)
 
-        col3Lbl = Label(win, text=self.table.heading(2)["text"])
-        col3Ent = Entry(win)
-        col3Ent.insert(0, values[2])  # Default is column 3's current value
-        col3Lbl.grid(row=0, column=2)
-        col3Ent.grid(row=1, column=2)
+        lblBookStatus = Label(win, text=self.table.heading(2)["text"])
+        entBookStatus = Entry(win)
+        # Default is column 3's current value
+        entBookStatus.insert(0, values[2])
+        lblBookStatus.grid(row=0, column=2)
+        entBookStatus.grid(row=1, column=2)
 
-        col4Lbl = Label(win, text=self.table.heading(3)["text"])
-        col4Ent = Entry(win)
-        col4Ent.insert(0, values[3])  # Default is column 3's current value
-        col4Lbl.grid(row=0, column=3)
-        col4Ent.grid(row=1, column=3)
+        lblBorrower = Label(win, text=self.table.heading(3)["text"])
+        entBorrower = Entry(win)
+        entBorrower.insert(0, values[3])  # Default is column 3's current value
+        lblBorrower.grid(row=0, column=3)
+        entBorrower.grid(row=1, column=3)
 
-        col5Lbl = Label(win, text=self.table.heading(4)["text"])
-        col5Ent = Entry(win)
-        col5Ent.insert(0, values[4])  # Default is column 3's current value
-        col5Lbl.grid(row=0, column=4)
-        col5Ent.grid(row=1, column=4)
+        lblBorrowerSN = Label(win, text=self.table.heading(4)["text"])
+        entBorrowerSN = Entry(win)
+        # Default is column 3's current value
+        entBorrowerSN.insert(0, values[4])
+        lblBorrowerSN.grid(row=0, column=4)
+        entBorrowerSN.grid(row=1, column=4)
 
-        col6Lbl = Label(win, text=self.table.heading(5)["text"])
-        col6Ent = Entry(win)
-        col6Ent.insert(0, values[5])  # Default is column 3's current value
-        col6Lbl.grid(row=0, column=5)
-        col6Ent.grid(row=1, column=5)
+        lbBorrowerTime = Label(win, text=self.table.heading(5)["text"])
+        entBorrowerTime = Entry(win)
+        # Default is column 3's current value
+        entBorrowerTime.insert(0, values[5])
+        lbBorrowerTime.grid(row=0, column=5)
+        entBorrowerTime.grid(row=1, column=5)
 
-        col7Lbl = Label(win, text=self.table.heading(6)["text"])
-        col7Txt = Text(win, height=10)
-        col7Txt.insert(END, values[6])
+        lblComments = Label(win, text=self.table.heading(6)["text"])
+        txtComments = Text(win, height=10)
+        txtComments.insert(END, values[6])
         # col7Ent.insert(0, values[6])  # Default is column 3's current value
-        col7Lbl.grid(row=2, column=2)
-        col7Txt.grid(row=3, column=0, columnspan=6)
+        lblComments.grid(row=2, column=2)
+        txtComments.grid(row=3, column=0, columnspan=6)
 
         def ConfirmEntry(self, treeView, entry1, entry2, entry3):
             ####
@@ -132,8 +135,11 @@ class booktreeview ():
             return True
 
         def UpdateThenDestroy(self):
-            if ConfirmEntry(self.table, col1Ent.get(), col2Ent.get(), col3Ent.get()):
+            if ConfirmEntry(self.table, entBookSN.get(), entBookStatus.get(), txtComments.get()):
                 win.destroy()
+
+        def DeleteThenDestroy():
+            pass
 
         okButt = Button(win, text="确定修改")
         okButt.bind("<Button-1>", lambda e: self.UpdateThenDestroy())
@@ -144,5 +150,5 @@ class booktreeview ():
         canButt.grid(row=4, column=2)
 
         delButt = Button(win, text="删除记录")
-        delButt.bind("<Button-1>", lambda c: win.destroy())
+        delButt.bind("<Button-1>", lambda f: DeleteThenDestroy())
         delButt.grid(row=4, column=4)
